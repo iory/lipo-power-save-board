67c0b2d24dc4c0949b94d9098ee3cb9da10a0550
Iori Yanokura <ab.ioryz@gmail.com>
2024-11-11 13:48:52 +0900

    Seperate config parameteres

--- include/config.h
+++ include/config.h
@@ -1,3 +1,8 @@
 const char* ssid = "";   //your network SSID
 const char* password = "";   //your network password
 const char *GScriptId = "";
+
+constexpr int TIME_TO_SLEEP  = 10;        /* Time ESP32 will go to sleep (in seconds) */
+constexpr float pressureThreshold = -7.0;
+constexpr float maxPressureThreshold = -15.0;
+constexpr int historySize = 10;
--- src/suction_logger.cpp
+++ src/suction_logger.cpp
@@ -30,10 +30,6 @@ enum Mode {
   MODE_COUNT
 };
 
-constexpr int TIME_TO_SLEEP  = 10;        /* Time ESP32 will go to sleep (in seconds) */
-constexpr float pressureThreshold = -7.0;
-constexpr float maxPressureThreshold = -15.0;
-constexpr int historySize = 20;
 RTC_DATA_ATTR int mode = NOTHING;
 RTC_DATA_ATTR int bootCount = 0;
 RTC_DATA_ATTR float batteryVoltages[historySize];
